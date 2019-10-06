a1c2e4e2d1337d8663969ae63836a551a2da240c
Justinette2175 <justine.gagnepain@dynamicly.com>
2019-10-06 19:55:24 -0400

    added pages components

--- src/client/App.js
+++ src/client/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  componentDidMount() {
+    // fetch('/api/sprints')
+    //   .then((data) => data.json())
+    //   .then((sprints) => {
+    //     console.log('sprints are', sprints)
+    //   });
+    fetch('/api/sprints/5d98e96b1a71c1280b447d31', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        options: { name: 'A new name for this sprint' },
+      }),
+    })
+      .then((data) => data.json())
+      .then((sprints) => {
+        console.log('sprints are', sprints);
+      });
+  }
+
   render() {
-    const { username } = this.state;
     return (
       <div>
         Loading.. please wait!
