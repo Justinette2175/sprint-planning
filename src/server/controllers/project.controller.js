beeff0b15f81efb22d82889fde0eeb975160cd9a
Justinette2175 <justine.gagnepain@dynamicly.com>
2019-10-13 18:04:39 -0400

    setup project controller

--- src/server/controllers/project.controller.js
+++ src/server/controllers/project.controller.js
@@ -2,15 +2,13 @@ import Project from '../models/project.model';
 
 
 exports.getProjects = (req, res) => {
-  // const sprintId = req.params.id;
-  // Sprint.findById(sprintId).then((sprint) => {
-  //   res.send(sprint);
-  // });
-}; 
+  const options = req.query;
+  Project.find(options, (err, projects) => res.send(projects));
+};
 
 exports.getProjectById = (req, res) => {
-  // const sprintId = req.params.id;
-  // Sprint.findById(sprintId).then((sprint) => {
-  //   res.send(sprint);
-  // });
+  const projectId = req.params.id;
+  Project.findById(projectId).then((project) => {
+    res.send(project);
+  });
 };
